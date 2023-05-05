1c65d047b1a2333d3f33201fab5635c8740e83da
dev-002 <devansh.gupta985@gmail.com>
2023-05-05 18:55:38 +0530

    Home and Login Route Setup

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+// Routes
+const homeRouter = require('./routes/homeRoute');
+const loginRouter = require('./routes/loginRoute');
+
+app.use('/', homeRouter);
+app.use('/login', loginRouter);
+
 app.listen(process.env.PORT, console.log(`Server Started at ${process.env.PORT}`));
