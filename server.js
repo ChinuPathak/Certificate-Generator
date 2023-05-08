8da81340e252f1b62f9ffc26f104c18e4edcd54d
dev-002 <devansh.gupta985@gmail.com>
2023-05-08 13:14:39 +0530

    Databse Connected

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
 
 const app = express();
 
-app.set('view engine','ejs');
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// const db = mongoose.connection;
+// db.on('error', (err) => console.log(err));
+// db.once('open', () => console.log('Database Connected'));
+
+app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-const bodyParseUser = (bodyParse.urlencoded({extended: true}));
+const bodyParseUser = (bodyParse.urlencoded({ extended: true }));
 
 // Routes
 const homeRouter = require('./routes/homeRoute');
@@ -15,7 +24,7 @@ const loginRouter = require('./routes/loginRoute');
 const certificateRouter = require('./routes/certificateDash');
 
 app.use('/', homeRouter);
-app.use('/login', bodyParseUser,loginRouter);
-app.use('/certificate',bodyParseUser,certificateRouter);
+app.use('/login', bodyParseUser, loginRouter);
+app.use('/certificate', bodyParseUser, certificateRouter);
 
 app.listen(process.env.PORT, console.log(`Server Started at ${process.env.PORT}`));
